8562d7d814d816eb5c11d156117e8261f534f784
Sam Makhlouf <maksam7dev@gmail.com>
2023-02-26 14:28:50 +0100

    selection popup swicking

--- app/stories/story/layout.tsx
+++ app/stories/story/layout.tsx
@@ -4,15 +4,8 @@ export default function StoryLayout({ children }: { children: React.ReactNode })
    return (
       <>
          <AdminSideNav>
-            <div className="bg-gray-200 h-full p-4 relative overflow-y-auto">
-               <div className="mx-auto max-w-7xl">
-
-              
-
-               {children}
-
-
-            </div>
+            <div className="bg-gray-200 h-full relative overflow-y-auto">
+               <div className="mx-auto max-w-7xl">{children}</div>
             </div>
          </AdminSideNav>
       </>
--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -3,9 +3,16 @@
 import React, { useRef, useState, useEffect } from "react"
 import { useQuill } from "react-quilljs"
 import "quill/dist/quill.snow.css"
-import rangy from "rangy/lib/rangy-core"
 import "rangy/lib/rangy-classapplier"
 import { TiUserAdd } from "react-icons/ti"
+import { MdEditLocationAlt, MdEditNote } from "react-icons/md"
+import { IconType } from "react-icons"
+
+const rangy = require("rangy/lib/rangy-core")
+
+type SetBoundType = { top: number; left: number; height: number; width: number; words: string[] }
+
+type PopupItemsType = ({ label: string; icon: IconType; handler: () => void } | null | undefined)[]
 
 const modules = {
    toolbar: [
@@ -16,7 +23,7 @@ const modules = {
       [{ indent: "-1" }, { indent: "+1" }],
 
       [{ header: [1, 2, 3, 4, false] }],
-      ["link", "image"],
+      ["link"],
       [{ color: [] }, { background: [] }],
 
       ["clean"],
@@ -26,20 +33,13 @@ const modules = {
    },
 }
 
-
-
 export default function TextEditor() {
    const { quill, quillRef } = useQuill({ modules })
-   const [selectBound, setSelectBound] = useState<number | { top: number; left: number, height:number, width:number, words:string[] }>(0)
+   const [selectBound, setSelectBound] = useState<number | SetBoundType>(0)
    let [isOpen, setIsOpen] = useState(false)
 
-   const setApplier = () => {
-      rangy
-         .createClassApplier("text-red-500", {
-            elementTagName: "span",
-            elementAttributes: { style: "color:red", "data-char": "character" },
-         })
-         .toggleSelection()
+   const setApplier = (markerClass: string, elementAttributes: any) => {
+      rangy.createClassApplier(markerClass, { elementAttributes }).toggleSelection()
    }
 
    useEffect(() => {
@@ -51,7 +51,7 @@ export default function TextEditor() {
 
       if (quill) {
          // initciall text
-         quill.clipboard.dangerouslyPasteHTML("<h1>React Hook for Quill!</h1>")
+         quill.clipboard.dangerouslyPasteHTML("<h1>React Hook for Quill! React Hook for Quill!</h1>")
 
          quill.on("selection-change", (range, oldRange, src) => {
             if (range) {
@@ -61,12 +61,13 @@ export default function TextEditor() {
                } else {
                   var text = quill.getText(range.index, range.length)
                   const bound = quill.getBounds(range.index, range.length)
-                  
-                  const words = text.split(" ")
+
+                  const words = text.split(" ").filter((el) => el)
                   //console.log(words)
-                  
-                  setSelectBound({...bound, words})
-                  
+
+                  const newBound = words.length > 0 ? { ...bound, words } : 0
+                  setSelectBound(newBound)
+
                   //console.log("User has highlighted", text)
                   //console.log(bound)
                }
@@ -84,56 +85,64 @@ export default function TextEditor() {
       }
    }, [quill, quillRef])
 
-   const popupItems = [
-      {
-         label: "Link Character",
-         icon: TiUserAdd,
-         handler: () => {
-            setIsOpen(true)
-            console.log(isOpen)
-         },
-      },
-      { label: "Link Place", icon: TiUserAdd, handler: () => console.log("Place") },
-      { label: "Create Note", icon: TiUserAdd, handler: () => console.log("Note") },
+   const linkWords = (linkTo: "character" | "place", dataAt: string | number) => {
+      let dataName = `data-${linkTo}`
+      setApplier(`marked-${linkTo}`, { style: "color:inherit", [dataName]: dataAt })
+   }
+
+   const popupItems: PopupItemsType = [
+      { label: "Link Character", icon: TiUserAdd, handler: () => linkWords("character", "chara00001") },
+      { label: "Link Place", icon: MdEditLocationAlt, handler: () => linkWords("place", "place00001") },
+      { label: "Create Note", icon: MdEditNote, handler: () => console.log("Note") },
    ]
-   //
+      .map((x, i) => {
+         if (typeof selectBound === "object") {
+            if (!(selectBound.words.length < 5)) {
+               return i == 2 ? x : null
+            } else {
+               return x
+            }
+         }
+      })
+      .filter((el) => el)
+
    return (
       <>
-         
-      
-         
          <div className="mx-auto max-w-2xl">
+            <h1 className="text-3xl my-5">Edit Single Chapter</h1>
 
-         
-         <div className="quill">
-            <div ref={quillRef} />
-
-
-         {typeof selectBound === "object" ? (
-            <div
-               style={{ top: selectBound.top - selectBound.height, left: selectBound.left + (selectBound.width / 2), transform:"translate(-50%,-50%)" }}
-               className="absolute z-10 flex item-center bg-white rounded-xl border border-gray-300 overflow-hidden"
-            >
-               {popupItems.map((item) => (
-                  <button
-                     key={item.label}
-                     title={item.label}
-                     aria-label={item.label}
-                     onClick={item.handler}
-                     className="p-3 text-gray-600 hover:bg-slate-200 shadow-2xl hover:text-emerald-500"
-                  >
-                     <item.icon size={"1.2rem"} />
-                  </button>
-               ))}
+            <div className="quill">
+               <div ref={quillRef} />
+               {typeof selectBound === "object" ? (
+                  <SelectionPop popupItems={popupItems} selectBound={selectBound} />
+               ) : null}
             </div>
-         ) : null}
-
          </div>
-         </div>
-         
       </>
    )
 }
 
-
-
+const SelectionPop = ({ popupItems, selectBound }: { popupItems: PopupItemsType; selectBound: SetBoundType }) => (
+   <>
+      <div
+         style={{
+            top: selectBound.top - selectBound.height,
+            left: selectBound.left + selectBound.width / 2,
+            transform: "translate(-50%,-50%)",
+         }}
+         className="absolute z-10 flex item-center bg-white/90 rounded-xl border border-gray-300 overflow-hidden"
+      >
+         {popupItems.map((item) => (
+            <button
+               key={item?.label}
+               title={item?.label}
+               aria-label={item?.label}
+               onClick={item?.handler}
+               className="p-3 text-gray-600 hover:bg-slate-200 shadow-2xl hover:text-emerald-500"
+            >
+               {item && <item.icon size={"1.2rem"} />}
+            </button>
+         ))}
+      </div>
+   </>
+)
